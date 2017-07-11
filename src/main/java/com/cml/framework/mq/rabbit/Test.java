1c71957fd3e5ef07c99f197f785c7c6405648a8b
menglin.chen <menglin.chen>
2017-07-11 18:27:21 +0800

    消息队列 rabbitmq 添加

---
+++ src/main/java/com/cml/framework/mq/rabbit/Test.java
@@ -0,0 +1,36 @@
+package com.cml.framework.mq.rabbit;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class Test {
+	// 队列名称
+	private final static String QUEUE_NAME = "hello";
+
+	public static void main(String[] argv) throws Exception {
+		/**
+		 * 创建连接连接到MabbitMQ
+		 */
+		ConnectionFactory factory = new ConnectionFactory();
+		// 设置MabbitMQ所在主机ip或者主机名
+		factory.setHost("192.168.99.100");
+		factory.setUsername("guest");
+		factory.setPassword("guest");
+		factory.setPort(5672);
+		// 创建一个连接
+		Connection connection = factory.newConnection();
+		// 创建一个频道
+		Channel channel = connection.createChannel();
+		// 指定一个队列
+		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+		// 发送的消息
+		String message = "hello world!"+System.currentTimeMillis();
+		// 往队列中发出一条消息
+		channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+		System.out.println(" [x] Sent '" + message + "'");
+		// 关闭频道和连接
+		channel.close();
+		connection.close();
+	}
+}
--- src/test/java/com/cml/framework/test/UUidTest.java
+++ src/test/java/com/cml/framework/test/UUidTest.java
@@ -5,7 +5,7 @@ import java.util.UUID;
 public class UUidTest {
 	public static void main(String[] args) {
 		System.out.println((0.3 * 100 - 0.2 * 100) / 100);
-		System.out.println(0.6 - 0.4);
+		System.out.println(1.6 - 0.4);
 		System.out.println(0.5 - 0.4);
 		System.out.println(0.4 - 0.3);
 		System.out.println(0.3 - 0.2);
