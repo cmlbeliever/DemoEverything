b38f97acf0159acdf21658291240f722e88cc448
menglin.chen <menglin.chen>
2017-07-11 15:30:06 +0800

    浮点运算踩坑

---
+++ src/test/java/com/cml/framework/test/UUidTest.java
@@ -0,0 +1,16 @@
+package com.cml.framework.test;
+
+import java.util.UUID;
+
+public class UUidTest {
+	public static void main(String[] args) {
+		System.out.println((0.3 * 100 - 0.2 * 100) / 100);
+		System.out.println(0.6 - 0.4);
+		System.out.println(0.5 - 0.4);
+		System.out.println(0.4 - 0.3);
+		System.out.println(0.3 - 0.2);
+		System.out.println(0.2 - 0.1);
+		System.out.println(0.1 - 0);
+		System.out.println(UUID.randomUUID().toString().length());
+	}
+}
