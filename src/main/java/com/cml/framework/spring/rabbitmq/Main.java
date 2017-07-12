3cb559cc24f52c37a1472adb9b644ebe6be8430e
menglin.chen <menglin.chen>
2017-07-12 17:50:47 +0800

    messagequene 添加ok

--- src/main/java/com/cml/framework/spring/rabbitmq/Main.java
+++ src/main/java/com/cml/framework/spring/rabbitmq/Main.java
@@ -3,12 +3,16 @@ package com.cml.framework.spring.rabbitmq;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
 				"classpath:com/cml/framework/spring/rabbitmq/application-rabbitmq-tag.xml");
 		MailService service = context.getBean(MailService.class);
 		System.out.println("before send");
-		service.sendMsg("from", "to", "sendMsg");
+		for (int i = 0; i < 10; i++) {
+			service.sendMsg("from", "to", "sendMsgXXXX" + i);
+			Thread.sleep(2000);
+		}
 		System.out.println("send success");
+		context.close();
 	}
 }
--- src/main/java/com/cml/framework/spring/rabbitmq/MessageReceiver.java
+++ src/main/java/com/cml/framework/spring/rabbitmq/MessageReceiver.java
@@ -1,13 +1,24 @@
 package com.cml.framework.spring.rabbitmq;
 
 import org.springframework.amqp.core.Message;
-import org.springframework.amqp.core.MessageListener;
+import org.springframework.amqp.rabbit.core.ChannelAwareMessageListener;
 
-public class MessageReceiver implements MessageListener {
+import com.rabbitmq.client.Channel;
+
+public class MessageReceiver implements ChannelAwareMessageListener {
 
 	@Override
-	public void onMessage(Message message) {
-		System.out.println("MessageReceiver.onReceiveMessage:" + new String(message.getBody()));
+	public void onMessage(Message message, Channel channel) throws Exception {
+		String msg = new String(message.getBody());
+
+		try {
+			System.out.println("MessageReceiver.onReceiveMessage:" + msg);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// 确认消息
+			channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+		}
 	}
 
 }
