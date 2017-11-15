d6add3079347df43e84bc76c551584e70d80477f
cml <menglin.chen>
2017-11-15 10:26:25 +0800

    添加令牌桶算法

---
+++ src/main/java/com/cml/framework/guava/RateLimitTest.java
@@ -0,0 +1,18 @@
+package com.cml.framework.guava;
+
+import com.google.common.util.concurrent.RateLimiter;
+
+public class RateLimitTest {
+	public static void main(String[] args) {
+		RateLimiter rateLimiter = RateLimiter.create(1);
+		System.out.println(rateLimiter.tryAcquire());
+		System.out.println(rateLimiter.tryAcquire());
+		System.out.println(rateLimiter.tryAcquire());
+		System.out.println(rateLimiter.acquire());
+		System.out.println(rateLimiter.acquire());
+		System.out.println(rateLimiter.acquire());
+		System.out.println(rateLimiter.acquire());
+		
+		Lea
+	}
+}
