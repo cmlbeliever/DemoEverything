737880e696da0c1684be425e56d5200e39480219
chenmenglin <you@example.com>
2018-04-26 10:08:02 +0800

    update

--- CommonDemo/src/main/java/com/cml/framework/redis/redission/RedissionTest.java
+++ CommonDemo/src/main/java/com/cml/framework/redis/redission/RedissionTest.java
@@ -1,14 +1,19 @@
 package com.cml.framework.redis.redission;
 
+import java.util.concurrent.TimeUnit;
+
 import org.redisson.Redisson;
 import org.redisson.api.RAtomicLong;
 import org.redisson.api.RLock;
 import org.redisson.api.RMapCache;
 import org.redisson.api.RedissonClient;
+import org.redisson.api.map.event.EntryEvent;
+import org.redisson.api.map.event.EntryExpiredListener;
+import org.redisson.api.map.event.MapEntryListener;
 import org.redisson.config.Config;
 
 public class RedissionTest {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		Config config = new Config();
 		config.useSingleServer().setAddress("redis://192.168.99.100:6379");
 		RedissonClient redisson = Redisson.create(config);
@@ -31,6 +36,24 @@ public class RedissionTest {
 		System.out.println("获得锁对象：" + rlock.getName());
 
 		rlock.unlock();
+
+		new Thread(() -> {
+			for (int i = 0; i < 10; i++) {
+				final String key = "newTest" + i;
+				RMapCache<String, String> cache = redisson.getMapCache(key);
+				long ttl = i > 7 ? 1 : 50;
+				cache.put("testV" + i, "xxx", ttl, TimeUnit.SECONDS);
+				cache.addListener(new EntryExpiredListener<String, String>() {
+
+					@Override
+					public void onExpired(EntryEvent<String, String> event) {
+						System.out.println("====>expire:" + event.getKey() + ",key:" + key + ",value:" + event.getValue());
+					}
+				});
+			}
+		}).start();
+
+		Thread.sleep(20000);
 
 		redisson.shutdown();
 	}
