e54ac3ba93a411ad61c5768e1fbc700782e0fc8c
chenmenglin <you@example.com>
2018-05-10 10:37:14 +0800

    添加redis list测试

---
+++ CommonDemo/src/main/java/com/cml/framework/redis/redission/RedissionQueneTest.java
@@ -0,0 +1,54 @@
+package com.cml.framework.redis.redission;
+
+import org.redisson.Redisson;
+import org.redisson.api.RList;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+public class RedissionQueneTest {
+    public static void main(String[] args) {
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://192.168.99.100:6379");
+        RedissonClient redisson = Redisson.create(config);
+        listTest(redisson);
+    }
+
+    private static void listTest(RedissonClient redisson) {
+        RList rList = redisson.getList("redisList");
+
+        new Thread(() -> {
+            try {
+                while (true) {
+                    System.out.println("当前队列长度：" + rList.sizeAsync().get());
+                    TimeUnit.SECONDS.sleep(1);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        System.out.println("初始化数量：" + rList.size());
+
+        long start = System.currentTimeMillis();
+        int len = 10_0000;
+        for (int i = 0; i < len; i++) {
+            rList.add(UUID.randomUUID().toString());
+        }
+        System.out.println("耗时：" + (System.currentTimeMillis() - start));
+        System.out.println("数量：" + rList.size());
+
+        System.out.println("=----pop-----------");
+        start = System.currentTimeMillis();
+        for (int i = 0; i < len; i++) {
+            rList.fastRemove(0);
+        }
+        System.out.println("移除耗时：" + (System.currentTimeMillis() - start));
+        System.out.println("数量：" + rList.size());
+    }
+}
